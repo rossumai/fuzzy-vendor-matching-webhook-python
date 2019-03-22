e8eb56134e9e0a8fd32f134514dec05e4d91d323
evelyn9191 <ockova.michaela@gmail.com>
2019-03-22 15:33:58 +0100

    Connector authentication token added

--- config.py
+++ config.py
@@ -7,4 +7,4 @@ DATABASE_CONFIG = {'host': 'ADD_HOST_NAME_HERE',
                    'db_name': 'ADD_DATABASE_NAME_HERE'}
 
 # Connector authentication key
-CONNECTOR_AUTH_KEY = {'CONNECTOR_AUTH_KEY': 'ADD_CONNECTOR_SECRET_AUTHENTICATION_KEY_HERE'}
+CONNECTOR_AUTH_KEY = {'CONNECTOR_AUTH_KEY': 'g44hrr61seohd58en3889yp8mnpjir95f1j07o0qybyw6t4cd8zdaxldsrf8ug21'}
